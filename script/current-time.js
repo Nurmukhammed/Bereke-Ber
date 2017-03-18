b82a9b72a8fc5908714d5814043f2a3b9ca5019e
Nurmukhammed Duisekeyev <nureke92@gmail.com>
2017-03-18 15:00:02 +0600

    Updated current time js

--- script/current-time.js
+++ script/current-time.js
@@ -3,22 +3,12 @@ var month = today.getMonth() + 1;
 var date = today.getDate();
 var year = today.getFullYear();
 
-function currentTime (date, month) {
-	if (month < 10) {
-	month = '0' + month;
-	return month;
+function addZero (x) {
+	if (x < 10) {
+		return '0' + x;
 	}
-	if (month == 10) {
-	return month;
-	}
-	if (date < 10) {
-	date = '0' + date;
-	return date;
-	}
-	if (date == 10) {
-	return date;
-	}
+	return x;
 }
 
 document.getElementById('show-date').innerHTML =
-' на ' + currentTime(date) + '.' + currentTime(month) + '.' + year;
+' на ' + addZero(date) + '.' + addZero(month) + '.' + year;
